2a3b8386ce7687b76bd246e40d610716755821c0
Pedro Leite <contato.pedro77@gmail.com>
2024-01-01 21:37:23 -0300

    Project created

---
+++ src/test/java/tech/pedroleite/deliveryapi/DeliveryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.pedroleite.deliveryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
